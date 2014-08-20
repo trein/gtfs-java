7d0fae326476672ac90f9d3840ef37792dbfd4e6
Guilherme M. Trein <guitrein@yahoo.com.br>
2014-08-20 07:15:38 -0400

    Adjust resources path

--- gtfs-etl/src/main/java/com/trein/gtfs/etl/config/JpaEtlConfig.java
+++ gtfs-etl/src/main/java/com/trein/gtfs/etl/config/JpaEtlConfig.java
@@ -17,36 +17,36 @@ import com.trein.gtfs.jpa.JpaRepositoryConfig;
 @Configuration
 @Import({ JpaRepositoryConfig.class, EtlConfig.class })
 public class JpaEtlConfig {
-    
-    private static final String GTFS_BASE_DIR = "br_poa/";
+
+    private static final String GTFS_BASE_DIR = "gtfs-data/br_poa/";
     public static final String STEP_NAME = "jpa_unique_step";
     public static final String JOB_NAME = "jpa_parser_job";
     private static final int CHUNK_SIZE = 1000;
-    
+
     @Autowired
     private JobBuilderFactory jobs;
-    
+
     @Autowired
     private StepBuilderFactory steps;
-    
+
     @Bean(name = JOB_NAME)
     public Job jpaJob(GtfsItemReader reader, GtfsJpaItemWriter writer) {
         return this.jobs.get(JOB_NAME).flow(jpaStep(reader, writer)).end().build();
     }
-    
+
     @Bean
     public Step jpaStep(GtfsItemReader reader, GtfsJpaItemWriter writer) {
         return this.steps.get(STEP_NAME).<Object, GtfsItem> chunk(CHUNK_SIZE).reader(reader).writer(writer).build();
     }
-
+    
     @Bean
     public GtfsJpaItemWriter jpaWriter() {
         return new GtfsJpaItemWriter();
     }
-    
+
     @Bean
     public GtfsItemReader reader() {
         return new GtfsItemReader(GTFS_BASE_DIR);
     }
-
+    
 }
--- gtfs-etl/src/main/java/com/trein/gtfs/etl/config/MongoEtlConfig.java
+++ gtfs-etl/src/main/java/com/trein/gtfs/etl/config/MongoEtlConfig.java
@@ -17,36 +17,36 @@ import com.trein.gtfs.mongo.MongoRepositoryConfig;
 @Configuration
 @Import({ MongoRepositoryConfig.class, EtlConfig.class })
 public class MongoEtlConfig {
-
-    private static final String GTFS_BASE_DIR = "br_poa/";
+    
+    private static final String GTFS_BASE_DIR = "gtfs-data/br_poa/";
     public static final String STEP_NAME = "mongo_unique_step";
     public static final String JOB_NAME = "mongo_parser_job";
     private static final int CHUNK_SIZE = 1000;
-
+    
     @Autowired
     private JobBuilderFactory jobs;
-
+    
     @Autowired
     private StepBuilderFactory steps;
-
+    
     @Bean(name = JOB_NAME)
     public Job mongoJob(GtfsItemReader reader, GtfsMongoItemWriter writer) {
         return this.jobs.get(JOB_NAME).flow(mongoStep(reader, writer)).end().build();
     }
-
+    
     @Bean
     public Step mongoStep(GtfsItemReader reader, GtfsMongoItemWriter writer) {
         return this.steps.get(STEP_NAME).<Object, GtfsItem> chunk(CHUNK_SIZE).reader(reader).writer(writer).build();
     }
-    
+
     @Bean
     public GtfsMongoItemWriter mongoWriter() {
         return new GtfsMongoItemWriter();
     }
-
+    
     @Bean
     public GtfsItemReader mongoReader() {
         return new GtfsItemReader(GTFS_BASE_DIR);
     }
-    
+
 }
